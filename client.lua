17ca17e240c0aae0eed755476afc6aeda2793647
NeenGame <neengame@abv.bg>
2023-07-24 08:33:01 +0300

    Update

--- client.lua
+++ client.lua
@@ -1,19 +1,23 @@
 CreateThread(function()
     while true do
+        local ped = PlayerPedId()
+        local armed = IsPedArmed
         local CamFov = GetGameplayCamFov()
         local CamFovCar = GetFinalRenderedCamFov()
         local CamPos = GetFollowPedCamViewMode()
-        local isinveh = IsPedInAnyVehicle(PlayerPedId(), false)
-        if isinveh and CamPos == 0 and CamFovCar > 60.1 then
-            TriggerServerEvent('neen-antiflawless:server:kick:vinka', CamFov)
-        elseif not isinveh and CamPos == 0 and CamFov > 60.2 then
-            TriggerServerEvent('neen-antiflawless:server:kick:vinka', CamFov)
-        elseif not isinveh and CamPos == 4 and CamFov > 58.0 then
-            TriggerServerEvent('neen-antiflawless:server:kick:vinka', CamFov)
-        elseif CamFov < 39.0 then
-            TriggerServerEvent('neen-antiflawless:server:kick:vinka', CamFov)
-        elseif isinveh and CamPos == 4 and CamFovCar > 60.6 then
-            TriggerServerEvent('neen-antiflawless:server:kick:vinka', CamFov)
+        local isinveh = IsPedInAnyVehicle(ped, false)
+        if armed(ped, 1) or armed(ped, 2) or armed(ped, 4) then
+            if isinveh and CamPos == 0 and CamFovCar > 60.1 then
+                TriggerServerEvent('neen-antiflawless:server:kick:vinka', CamFov)
+            elseif not isinveh and CamPos == 0 and CamFov > 60.2 then
+                TriggerServerEvent('neen-antiflawless:server:kick:vinka', CamFov)
+            elseif not isinveh and CamPos == 4 and CamFov > 58.0 then
+                TriggerServerEvent('neen-antiflawless:server:kick:vinka', CamFov)
+            elseif CamFov < 39.0 then
+                TriggerServerEvent('neen-antiflawless:server:kick:vinka', CamFov)
+            elseif isinveh and CamPos == 4 and CamFovCar > 60.6 then
+                TriggerServerEvent('neen-antiflawless:server:kick:vinka', CamFov)
+            end
         end
         Wait(1500)
     end
--- server.lua
+++ server.lua
@@ -1,3 +1,18 @@
 RegisterNetEvent('neen-antiflawless:server:kick:vinka', function(fov)
     DropPlayer(source, 'Flawless Widescreen Detected fov: ' ..fov)
 end)
+
+local function CheckVersion()
+	PerformHttpRequest('https://raw.githubusercontent.com/NeenGame/neen-antiflawless/master/version.txt', function(err, newestVersion, headers)
+		local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version')
+		if not newestVersion then print("Currently unable to run a version check.") return end
+		local advice = "^1You are currently running an outdated version^7, ^1please update^7"
+		if newestVersion:gsub("%s+", "") == currentVersion:gsub("%s+", "") then 
+            advice = '^6neen-antiflawless is up to date^7'
+		else
+            print("^3Version Check^7: \n^2Current^7: "..currentVersion.."\n^2Latest^7: "..newestVersion .. '\n^2Github^7: https://github.com/NeenGame/neen-antiflawless') 
+        end
+		print(advice)
+	end)
+end
+CheckVersion()
