c976b7030b6f433de2a2a23dd09d86e6fdf36976
NeenGame <neengame@abv.bg>
2023-07-26 09:15:30 +0300

    version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'NeenGame'
-version "0.1.0"
+version "0.1.1"
 description 'neen-antiflawless is a game-changing FiveM script designed to uphold fairness and integrity in gameplay by putting an end to the exploitation of flawless widescreen configurations and FOV abuse.'
 lua54 'yes'
 
